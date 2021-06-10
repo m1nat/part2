08273017239ee3ec3d6b797a4b7d5d6f2bbb685d
Vitaliy <Mushpakov.Vitaliy@gmail.com>
2021-06-10 12:56:36 +0300

    cw4

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-import { APIinit, createCars, getCars } from "./api/api-handlers.js";
-import moment from "moment";
+// import { APIinit, createCars, getCars } from "./api/api-handlers.js";
+// import moment from "moment";
 
-APIinit();
+// APIinit();
 // _______________________________________________________________________________________
-// const user = {
-//   // firstName: "Vitaliy",
-//   lastName: "Sinkevich",
-//   age: 25,
-//   job: {
-//     // company: "iTechArt",
-//     position: "webDev",
-//     skills: ["JS", "HTML", "CSS"],
-//   },
-// };
-
-// const { firstName: name = "noname, Your name has been faled" } = user;
-
-// const { job: { company: myCompany = 'Epam' } } = user;
-
-// console.log(myCompany, name);
-
-// console.log(user);
-// ______________________________________________________________________________________
-const car = {
-  car: 'Mercedes-Benz',
-  classes: 'GL-class',
-  year: 2013,
-  price: 29900,
-  color: 'black',
-  date: moment().format("MMMM Do YYYY, h:mm:ss a"),
+const user = {
+  // firstName: "Vitaliy",
+  lastName: "Sinkevich",
+  age: 25,
+  job: {
+    // company: "iTechArt",
+    position: "webDev",
+    skills: ["JS", "HTML", "CSS"],
+  },
 };
+
+  const { firstName: name = "noname, Your name has been faled",
+job : {company: myCompany = 'Epam'} }  = user;
+
+
+
+console.log(name, myCompany);
+
+// ______________________________________________________________________________________
+// const car = {
+//   car: 'Mercedes-Benz',
+//   classes: 'GL-class',
+//   year: 2013,
+//   price: 29900,
+//   color: 'black',
+//   date: moment().format("MMMM Do YYYY, h:mm:ss a"),
+// };
 
 // createCars(car);
 
-getCars()
+// getCars()
